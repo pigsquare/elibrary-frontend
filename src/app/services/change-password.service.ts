916d6e3cc4cb340dca01bf36f588c0c1dc8441c7
zhufz <zhufz3528ui@126.com>
2021-04-10 23:45:19 +0800

    fix: change password

--- src/app/services/change-password.service.ts
+++ src/app/services/change-password.service.ts
@@ -11,6 +11,6 @@ export class ChangePasswordService {
 
   constructor(private http: HttpClient) { }
   changeP(request: ChangePasswordRequest): Observable<CommonResponse>{
-    return this.http.post<CommonResponse>('/api/user/change/password', request);
+    return this.http.post<CommonResponse>('/api/user/password/change', request);
   }
 }
