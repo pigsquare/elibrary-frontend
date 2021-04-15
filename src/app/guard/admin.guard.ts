2ea7f020b288305e8aa78be7a1449f990d36af67
zhufz <zhufz3528ui@126.com>
2021-04-15 09:38:55 +0800

    feat: router and guard

---
+++ src/app/guard/admin.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanActivateChild {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return window.localStorage.getItem('user_role') === 'ROLE_ADMIN';
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return window.localStorage.getItem('user_role') === 'ROLE_ADMIN';
+  }
+
+}
---
+++ src/app/guard/reader.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReaderGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const res = window.localStorage.getItem('token') !== null;
+    if (res && (Number(window.localStorage.getItem('expr'))) < Date.now() - 60000){
+      this.authService.destroyToken();
+      alert('登录已过期，请重新登录！');
+      return false;
+    }
+    if (res && (Number(window.localStorage.getItem('expr'))) < Date.now() - 300000){
+        // TODO: 获取新token
+    }
+
+    return res;
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivate(childRoute, state);
+  }
+}
