0317f93b69a49945256b015fb1f9bd0a5dac8262
SophiaXu <1055199535@qq.com>
2021-04-16 20:25:46 +0800

    feat: add a logo and five buttons(already formatted) in homepage. But grid-list needs to be modified.

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -10,6 +15,12 @@ export class HomepageComponent implements OnInit {
 
   constructor(private router: Router) { }
   value = 'search';
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
   ngOnInit(): void {
   }
   navToIndex(): void{
