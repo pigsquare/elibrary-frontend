bae2cfe94f972673882eda9a3f5fe28351f978b4
zhufz <zhufz3528ui@126.com>
2021-04-17 00:49:47 +0800

    feat: title of homepage text area

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -14,7 +14,7 @@ export interface Tile {
 export class HomepageComponent implements OnInit {
 
   constructor(private router: Router) { }
-  value = 'search';
+  value = '';
   tiles: Tile[] = [
     {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
